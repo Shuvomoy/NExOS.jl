fb84a4c14c7aa8c2888ca94b97c9e6b9de76ef3c
Shuvomoy Das Gupta <shuvomoy.dasgupta@gmail.com>
2022-09-10 12:56:01 -0400

    Update large_scale_sparse_regression.jl

--- src/large_scale_sparse_regression.jl
+++ src/large_scale_sparse_regression.jl
@@ -2,9 +2,62 @@
 
 ## function that computes the tolerance for proximal evaluation for a given inner iteration number i
 
-function ϵProxGen(i::Int64; tolerance_type = :default)
+struct SettingsLSR
+
+    # first comes description of different data type
+
+    μ_max::Float64 # starting μ
+    μ_min::Float64 # ending μ
+    μ_mult_fact::Float64 # multiplicative factor to reduce μ
+    n_iter_min::Int64 # minimum number of iterations per fixed μ
+    n_iter_max::Int64 # maximum number of iterations per fixed μ
+    big_M::Float64 # a big number, it suffices to hard code it, but for now let me keep it
+    β::Float64 # β is the strong convexity parameter
+    tol::Float64 # tolerance for the fixed point gap
+    verbose::Bool # whether to print iteration information
+    freq::Int64 # how often to print the iteration solution
+    γ_updt_rule::Symbol # for now two rule: :safe and :adaptive, deafult will be :safe
+
+    # constructor function
+    function SettingsLSR(;
+        μ_max = 1.0,
+        μ_min = 1e-12,
+        μ_mult_fact = 0.85,
+        n_iter_min = 1000,
+        n_iter_max = 1000,
+        big_M = 1e99,
+        β = 1e-10,
+        tol = 1e-8,
+        verbose = true,
+        freq = 10,
+        γ_updt_rule = :safe
+        )
+        new(μ_max, μ_min, μ_mult_fact, n_iter_min, n_iter_max, big_M, β, tol, verbose, freq, γ_updt_rule)
+    end
+
+end
+
+##
+
+function γ_from_μ_LSR(μ::R, settings::SettingsLSR) where {R <: Real}
+    if settings.γ_updt_rule == :safe
+        return abs(cbrt(settings.μ_min)) # cbrt also works quite well
+    else
+        @error "either select :adaptive or :safe or :adaptiveplus as your γ selection rule"
+        return nothing
+    end
+end
+
+methods(γ_from_μ_LSR)
+
+##
+
+function ϵProxGenerate(i::Int64; tolerance_type = :default)
+    # @info "invoking ϵProxGenerate function"
     if tolerance_type == :default 
         return 1/(i+1)^1.5
+    elseif tolerance_type == :constant
+            return 1e-4       
     else
         @error "correct tolerance_type is not given"
     end
@@ -50,7 +103,7 @@ end
     ## this function constructs the very first state for the problem and settings for the factor analysis model
     # ========================================================= 
 
-function StateLargeSparseRegModel(problem::ProblemLargeSparseRegModel, settings::Settings)
+function StateLargeSparseRegModel(problem::ProblemLargeSparseRegModel, settings::SettingsLSR)
 
     # this function constructs the very first state for the problem and settings
     z0 = copy(problem.z0)
@@ -63,8 +116,8 @@ function StateLargeSparseRegModel(problem::ProblemLargeSparseRegModel, settings:
     fxd_pnt_gap = copy(settings.big_M)
     fsblt_gap = copy(settings.big_M)
     μ = copy(settings.μ_max)
-    γ = γ_from_μ(μ, settings)
-    ϵProx = ϵProxGen(i; tolerance_type = :default)
+    γ = abs(cbrt(settings.μ_min)) # γ_from_μ_LSR(μ, settings)
+    ϵProx = ϵProxGenerate(i; tolerance_type = :constant)
 
     return StateLargeSparseRegModel(x0Prev, y0Prev, z0Prev, x0, y0, z0, i, fxd_pnt_gap, fsblt_gap, μ, γ, ϵProx)
 
@@ -86,13 +139,13 @@ end
 ## Of course, we will need a constructor for the InitInfoLargeSparseRegModeltype, given the user input. The user inputs are problem::problem, settings::settings.
 # =========================================================
 
-function InitInfoLargeSparseRegModel(problem::ProblemLargeSparseRegModel, settings::Settings)
+function InitInfoLargeSparseRegModel(problem::ProblemLargeSparseRegModel, settings::SettingsLSR)
 
     ## constructs the initial NExOS information
     z0 = copy(problem.z0)
     μ = copy(settings.μ_max)
-    γ = γ_from_μ(μ, settings)
-    ϵProx = ϵProxGen(1; tolerance_type = :default)# copy(settings.ϵ_prox)
+    γ = abs(cbrt(settings.μ_min)) # γ_from_μ_LSR(μ, settings)
+    ϵProx = ϵProxGenerate(1; tolerance_type = :constant)# copy(settings.ϵ_prox)
     return InitInfoLargeSparseRegModel(z0, μ, γ, ϵProx)
 
 end
@@ -100,7 +153,7 @@ end
 ## update the state for large scale sparse regression model
 # =========================================================
 
-function update_state_lsr!(state::StateLargeSparseRegModel, init_info::InitInfoLargeSparseRegModel, problem::ProblemLargeSparseRegModel, settings::Settings)
+function update_state_lsr!(state::StateLargeSparseRegModel, init_info::InitInfoLargeSparseRegModel, problem::ProblemLargeSparseRegModel, settings::SettingsLSR)
 
     # extract information from the state and init info
     xPrev = state.xPrev
@@ -163,7 +216,7 @@ function update_state_lsr!(state::StateLargeSparseRegModel, init_info::InitInfoL
         # increment the count by 1
         i = i+1
         # update ϵProx 
-        ϵProx = ϵProxGen(i; tolerance_type = :default) 
+        ϵProx = ϵProxGenerate(i; tolerance_type = :constant) 
 
     end # while
 
@@ -179,7 +232,7 @@ function update_state_lsr!(state::StateLargeSparseRegModel, init_info::InitInfoL
     state.fsblt_gap = best_fsblt_gap
     state.μ = μ
     state.γ = γ
-    state.ϵProx = ϵProxGen(i; tolerance_type = :default) 
+    state.ϵProx = ϵProxGenerate(i; tolerance_type = :constant) 
 
     # Print information about the best point
     if settings.verbose == true
@@ -200,7 +253,7 @@ function inner_iteration_lsr(xPrev::V, yPrev::V, zPrev::V, x::V, y::V, z::V, β:
     yPrev = y
     zPrev = z
     x = prox_NExOS_lsr(problem.GMat, problem.hVec, γ, zPrev, xPrev, ϵProx, problem.PMat) # xPrev is used as the warm-starting point for the prox evaluation and ϵProx is the termination tolerance for cg!
-    y = Π_NExOS_lsr(k, Γ, β, γ, μ, 2*x - z)
+    y = Π_NExOS_lsr(problem.k, problem.Γ, β, γ, μ, 2*x - z)
     z = z + y - x
     # println("size of the new matrices = ", size(Z), size(z))
     return xPrev, yPrev, zPrev, x, y, z
@@ -252,22 +305,33 @@ end
 # =========================================================
 
 function prox_NExOS_lsr(GMat::A, hVec::V,  γ::R, zPrev::V, xPrev::V, ϵProx::R, PMat) where {R <: Real, A <: AbstractMatrix{R}, V <:  AbstractVector{R}}
-    sol = xPrev
-    cg!(sol, GMat, hVec + (1/γ).*zPrev; Pl = PMat, abstol = ϵProx)
+    sol = copy(xPrev)
+    # cg!(sol, GMat, hVec + (1/γ).*zPrev; Pl = PMat, abstol = 1e-4)
     # recall that: GMat ≡ (λ*A'*A) + (1/γ)*I
     #          and hVec ≡ λ*A'*b
+    Anew = [0.0026751207469390887 -1.7874010558441433 1.9570015234024314 0.48297821744005304 0.5499471860787609; -1.1926192117844148 -0.2962711608418759 0.34270253049651844 -0.9366799738457191 2.6135543879953094; -2.3956100254067567 1.5957363630260428 -0.5408329087542932 -1.595958552841769 -1.6414598361443185; 0.6791192541307551 2.895535427621422 -0.6676549207144326 -0.49424052539586016 -0.4336822064078515; 2.201382399088591 -0.578227952607833 -0.17602060199064307 -0.46261612338452723 0.24784285430931077; -0.49522601422578916 0.44636764792131967 1.2906418721153354 0.8301596930345838 0.5837176074011505];
+
+    bnew = [-0.7765194270735608, 0.28815810383363427, -0.07926006593887291, 0.5659412043036721, 1.3718921913877151, 0.8834692513807884];
+
+    sol1 = inv((2*Anew'*Anew) + (I/γ))*( (2*Anew'*bnew) + (zPrev ./ γ))
+
+    f = LeastSquares(Anew, bnew, iterative = true)
+    prox_point = similar(xPrev) # allocate prox_NExOS output
+    prox!(prox_point, f, zPrev, γ) # this is the prox! function from the ProximalOperators package
+    sol = prox_point
+    @show norm(sol1-sol)
     return sol
 end
 
 ## Function to update the initial information
 # ===========================================
 
-function update_init_info!(state::StateLargeSparseRegModel, init_info::InitInfoLargeSparseRegModel, problem::ProblemLargeSparseRegModel, settings::Settings)
+function update_init_info!(state::StateLargeSparseRegModel, init_info::InitInfoLargeSparseRegModel, problem::ProblemLargeSparseRegModel, settings::SettingsLSR)
 
     init_info.μ = init_info.μ*settings.μ_mult_fact
-    init_info.γ = γ_from_μ(init_info.μ, settings)
+    init_info.γ = abs(cbrt(settings.μ_min)) # γ_from_μ(init_info.μ, settings)
     init_info.z0 = state.z
-    init_info.ϵProx = ϵProxGen(1; tolerance_type = :default)
+    init_info.ϵProx = ϵProxGenerate(1; tolerance_type = :constant)
 
     return init_info
 
@@ -277,7 +341,7 @@ end
 # =====================
 
 # Dedicated solver for the large sparse regression problem
-function solve!(problem::ProblemLargeSparseRegModel, settings::Settings)
+function solve!(problem::ProblemLargeSparseRegModel, settings::SettingsLSR)
 
     # create the initial state
     state = StateLargeSparseRegModel(problem, settings) # create the initial state, keep in mind actually we can run a proximal evaluation now that we can use to warm start later
@@ -286,8 +350,6 @@ function solve!(problem::ProblemLargeSparseRegModel, settings::Settings)
     #create the optimization problem to compute the proximal operator
 
     # now this first state goes into the iteration_outer!(state, problem, settings) and we keep running it until our termination condtion has been met
-
-    μ_min = 1e-4
     
     while state.μ >= μ_min # settings.μ_min
         # run the inner iteration algorithm update procedure
